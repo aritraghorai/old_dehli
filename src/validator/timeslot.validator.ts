622e37db26c6b1eb60a7ce8b9bba3339a517dcd1
Aritra Ghorai <aritra.ghorai@zemosolabs.com>
2024-03-26 23:04:44 +0530

    added day js support

--- src/validator/timeslot.validator.ts
+++ src/validator/timeslot.validator.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod';
+import dayjs from 'dayjs';
 
 export const TimeSlotRequestSchema = z.object({
-  startTime: z.coerce.date().transform(date => new Date(date)),
-  endTime: z.coerce.date().transform(date => new Date(date)),
+  startTime: z.coerce.date().transform(date => dayjs(date).toDate()),
+  endTime: z.coerce.date().transform(date => dayjs(date).toDate())
 });
 
 export type TimeSlotRequest = z.infer<typeof TimeSlotRequestSchema>;
